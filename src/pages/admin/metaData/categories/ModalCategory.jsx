16b98d4098fb2eade7a26d21a278021e15f92c78
Nguyen Van Duy Manh <duymanhdnvn20092005@gmail.com>
2026-04-17 13:14:23 +0700

    Fix Style Modal

--- src/pages/admin/metaData/categories/ModalCategory.jsx
+++ src/pages/admin/metaData/categories/ModalCategory.jsx
@@ -1,72 +1,62 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 import Slide from '@mui/material/Slide';
-import { TextField } from '@mui/material';
 import { FaSpinner } from 'react-icons/fa';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function ModalCategory({ open, onChangeInput, handleClose, addCategory, error, loading, category }) {
-
+export default function ModalCategory({open, onChangeInput, handleClose, addCategory, error, loading, category}) {
     return (
-        <React.Fragment>
-            <Dialog
-                open={open}
-                slots={{
-                    transition: Transition,
-                }}
-                keepMounted
-                onClose={handleClose}
-                aria-describedby="alert-dialog-slide-description"
-                role="alertdialog"
-            >
-                <DialogTitle> 
-                    {category.id ? "Update Category" : "Add Categoy"}
-                </DialogTitle>
-                <DialogContent>
-                    <TextField
-                        name='name'
-                        onChange={onChangeInput}
-                        fullWidth sx={{ mt: 2 }}
-                        label="Name"
-                        variant="outlined"
-                        value={category.name}
-                        helperText={error.name}
-                        error={!!error.name}
-                    />
-                    <TextField
-                        name='description'
-                        onChange={onChangeInput}
-                        fullWidth sx={{ mt: 2 }}
-                        multiline rows={3}
-                        label="Description"
-                        variant="outlined"
-                        value={category.description}
-                        helperText={error.description}
-                        error={!!error.description}
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} autoFocus>
-                        Disagree
-                    </Button>
-                    <Button disabled={loading} onClick={addCategory}>{loading ? (
-                        <FaSpinner className="spin" />
-                    ) : (
-                        
-                        category.id ? "UPDATE" : "ADD"
-                    )}</Button>
-                </DialogActions>
-            </Dialog>
-        </React.Fragment>
+        <Dialog
+            open={open}
+            slots={{ transition: Transition }}
+            keepMounted
+            onClose={handleClose}
+            className="modal-wrapper"
+            PaperProps={{ className: "modal-inner" }}
+            BackdropProps={{ className: "modal-backdrop-x" }}
+        >
+            <DialogTitle className="modal-header-x">
+                {category.id ? "Update Category" : "Add Category"}
+            </DialogTitle>
+
+            <DialogContent className="modal-body-x">
+                <TextField
+                    className="modal-input-x"
+                    name="name"
+                    onChange={onChangeInput}
+                    fullWidth
+                    label="Name"
+                    variant="outlined"
+                    value={category.name}
+                    helperText={error.name}
+                    error={!!error.name}
+                />
+                <TextField
+                    className="modal-input-x"
+                    name="description"
+                    onChange={onChangeInput}
+                    fullWidth
+                    multiline
+                    rows={3}
+                    label="Description"
+                    variant="outlined"
+                    value={category.description}
+                    helperText={error.description}
+                    error={!!error.description}
+                />
+            </DialogContent>
+
+            <DialogActions className="modal-actions-x">
+                <Button onClick={handleClose} className="btn-cancel-x">
+                    Cancel
+                </Button>
+                <Button disabled={loading} onClick={addCategory} className="btn-submit-x">
+                    {loading ? <FaSpinner className="spin" /> : category.id ? "UPDATE" : "ADD"}
+                </Button>
+            </DialogActions>
+        </Dialog>
     );
 }
-
-
