c46ba47f5b2864293637c65c2a4c28cd7665d350
Nguyen Van Duy Manh <duymanhdnvn20092005@gmail.com>
2026-06-11 21:55:03 +0700

    Update Footer Client

--- src/components/client/FooterClient.jsx
+++ src/components/client/FooterClient.jsx
@@ -1,68 +1,93 @@
 import React from 'react';
-import { FaFacebookF, FaLinkedinIn, FaStar } from 'react-icons/fa';
+import { FaApple, FaFacebookF, FaGooglePlay, FaLinkedinIn, FaStar } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
+import Logo from '../../assets/Logo.png';
+import Logo2 from '../../assets/Logo2.png';
 
 function FooterClient() {
     return (
-        <footer className='bg-[#070a14] px-8 py-12 text-white md:px-14 lg:px-20'>
-            <div className='max-w-200'>
-                <div className='mb-8 inline-flex items-center gap-2 rounded-full bg-red-900/90 px-4 py-2 text-sm font-semibold text-white shadow-[0_0_18px_rgba(127,29,29,0.35)] transition-all duration-300 hover:bg-red-800'>
-                    <span className='flex h-7 w-7 items-center justify-center rounded-full bg-red-600 text-yellow-300'>
-                        <FaStar className='text-sm' />
-                    </span>
-                    Hoàng Sa & Trường Sa là của Việt Nam!
-                </div>
-
-                <div className='flex flex-col gap-8 md:flex-row md:items-center'>
-                    <div className='flex items-center gap-4'>
-                        <img
-                            src="https://rophim10.app/images/logo.svg" className='h-18 w-auto object-contain'
-                        />
+        <footer className='bg-black px-8 py-12 text-white md:px-14 lg:px-20'>
+            <div className='flex max-w-full items-start justify-between gap-10'>
+                <div className='max-w-200'>
+                    <div className='mb-8 inline-flex items-center gap-2 rounded-full bg-red-900 px-4 py-2 text-sm font-semibold text-white'>
+                        <span className='flex h-7 w-7 items-center justify-center rounded-full bg-red-600 text-yellow-300'>
+                            <FaStar className='text-sm' />
+                        </span>
+                        Hoàng Sa & Trường Sa là của Việt Nam!
                     </div>
 
-                    <div className=' h-12 w-1px bg-white/10 md:block'></div>
+                    <div className='flex flex-col gap-8 md:flex-row md:items-center'>
+                        <div className='flex items-center gap-4'>
+                            <img src={Logo2} className='h-18 w-auto object-contain' />
+                        </div>
 
-                    <div className='flex items-center gap-4'>
-                        <button className='flex h-12 w-12 items-center justify-center rounded-full bg-white/8 text-lg text-white/80 transition-all duration-300 hover:-translate-y-1 hover:bg-white hover:text-black hover:shadow-[0_0_18px_rgba(255,255,255,0.25)]'>
-                            <FaXTwitter />
-                        </button>
+                        <div className='ml-10 flex items-center gap-4'>
+                            <a href="" target="_blank" className='flex h-12 w-12 items-center justify-center rounded-full bg-white/8 text-lg text-white/80 transition-all duration-300 hover:-translate-y-1 hover:bg-white hover:text-black hover:shadow-[0_0_18px_rgba(255,255,255,0.25)]'><FaXTwitter /></a>
 
-                        <button className='flex h-12 w-12 items-center justify-center rounded-full bg-white/8 text-lg text-white/80 transition-all duration-300 hover:-translate-y-1 hover:bg-blue-600 hover:text-white hover:shadow-[0_0_18px_rgba(37,99,235,0.35)]'>
-                            <FaFacebookF />
-                        </button>
+                            <a href="https://www.facebook.com/duymanhdev" target="_blank" className='flex h-12 w-12 items-center justify-center rounded-full bg-white/8 text-lg text-white/80 transition-all duration-300 hover:-translate-y-1 hover:bg-blue-600 hover:text-white hover:shadow-[0_0_18px_rgba(37,99,235,0.35)]'><FaFacebookF /></a>
 
-                        <button className='flex h-12 w-12 items-center justify-center rounded-full bg-white/8 text-lg text-white/80 transition-all duration-300 hover:-translate-y-1 hover:bg-blue-700 hover:text-white hover:shadow-[0_0_18px_rgba(29,78,216,0.35)]'>
-                            <FaLinkedinIn />
-                        </button>
+                            <a href="https://www.linkedin.com/in/duymanhdev/" target="_blank" className='flex h-12 w-12 items-center justify-center rounded-full bg-white/8 text-lg text-white/80 transition-all duration-300 hover:-translate-y-1 hover:bg-blue-700 hover:text-white hover:shadow-[0_0_18px_rgba(29,78,216,0.35)]'><FaLinkedinIn /></a>
+                        </div>
+                    </div>
+
+                    <div className='mt-8 flex flex-wrap gap-x-10 gap-y-4 text-base font-medium text-white'>
+                        <a href="" className='transition-all duration-300 hover:text-yellow-300'>
+                            Hỏi Đáp
+                        </a>
+
+                        <a href="" className='transition-all duration-300 hover:text-yellow-300'>
+                            Chính sách bảo mật
+                        </a>
+
+                        <a href="" className='transition-all duration-300 hover:text-yellow-300'>
+                            Điều khoản sử dụng
+                        </a>
+
+                        <a href="" className='transition-all duration-300 hover:text-yellow-300'>
+                            Giới thiệu
+                        </a>
+                    </div>
+
+                    <div className='mt-5 max-w-195 text-[15px] leading-7 text-gray-300'>
+                        <p>
+                            MFILM - Trang xem phim online miễn phí chất lượng cao Vietsub, thuyết minh, lồng tiếng Full HD – 4K. Kho phim mới khổng lồ gồm phim chiếu rạp, phim bộ, phim lẻ từ Việt Nam, Hàn Quốc, Trung Quốc, Thái Lan, Nhật Bản, Âu Mỹ… đa dạng thể loại.
+                            <br />
+                            Dịp Tết Việt Nam, RoPhim trở lại với loạt phim xuân hấp dẫn, hài Tết và siêu phẩm mới nhất, mang đến trải nghiệm giải trí trọn vẹn bên gia đình.
+                        </p>
+
+                        <p className='mt-4 text-gray-400'>
+                            2026 MFILM
+                        </p>
                     </div>
                 </div>
 
-                <div className='mt-10 flex flex-wrap gap-x-10 gap-y-4 text-base font-medium text-white'>
-                    <a href="" className='transition-all duration-300 hover:text-yellow-300'>
-                        Hỏi Đáp
-                    </a>
+                <div className='hidden flex-1 flex-col items-center lg:flex'>
+                    <div className='mb-8 max-w-150 text-center'>
+                        <h2 className='text-2xl font-bold uppercase leading-snug tracking-wide text-gray-300'>
+                            Tải ứng dụng MFILM trên thiết bị di động để trải nghiệm mượt mà hơn
+                        </h2>
 
-                    <a href="" className='transition-all duration-300 hover:text-yellow-300'>
-                        Chính sách bảo mật
-                    </a>
+                        <div className='mt-5 flex items-center justify-center gap-4'>
+                            <a href="" className='flex h-16 min-w-56 items-center gap-3 rounded-xl border border-white/60 bg-black px-5 text-white transition-all duration-300 hover:-translate-y-1 hover:border-white hover:shadow-[0_0_22px_rgba(255,255,255,0.22)]'>
+                                <FaApple className='text-4xl' />
+                                <div className='text-left leading-none'>
+                                    <p className='text-xs font-semibold'>Download on the</p>
+                                    <h3 className='text-2xl font-bold'>App Store</h3>
+                                </div>
+                            </a>
 
-                    <a href="" className='transition-all duration-300 hover:text-yellow-300'>
-                        Điều khoản sử dụng
-                    </a>
-
-                    <a href="" className='transition-all duration-300 hover:text-yellow-300'>
-                        Giới thiệu
-                    </a>
-                </div>
-
-                <div className='mt-12 max-w-195 text-[15px] leading-7 text-gray-300'>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta incidunt est numquam fuga nihil. Eum porro expedita eaque ullam illum quas vel aspernatur fuga, perspiciatis, totam quos nam. Sequi veritatis impedit sint dolores, ipsam, voluptatibus porro laboriosam ipsum, eius culpa minima aspernatur necessitatibus? Fuga repudiandae iure, laborum molestiae voluptatem provident.
-                    </p>
-
-                    <p className='mt-4 text-gray-400'>
-                        2025 Rổ Phim. All rights reserved.
-                    </p>
+                            <a href="" className='flex h-16 w-56 items-center gap-3 rounded-xl border border-white/60 bg-black px-5 text-white transition-all duration-300 hover:-translate-y-1 hover:border-white hover:shadow-[0_0_22px_rgba(255,255,255,0.22)]'>
+                                <FaGooglePlay className='text-3xl text-green-400' />
+                                <div className='text-left leading-none'>
+                                    <p className='text-xs font-semibold'>GET IT ON</p>
+                                    <h3 className='text-2xl font-bold'>Google Play</h3>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='flex w-full justify-center items-center'>
+                        <img src={Logo} alt="MFILM" className='w-110 object-contain' />
+                    </div>
                 </div>
             </div>
         </footer>
