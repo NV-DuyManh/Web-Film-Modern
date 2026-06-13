ffb6d025748fc539535a59c5bf8a3ba479b94469
Nguyen Van Duy Manh <duymanhdnvn20092005@gmail.com>
2026-06-13 14:01:06 +0700

    Update Slide Film Country

--- src/pages/client/slide/SlideFilmCountry.jsx
+++ src/pages/client/slide/SlideFilmCountry.jsx
@@ -1,102 +1,166 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
-import './SlideFilmCountry.css';
-
-import { Pagination, Navigation } from 'swiper/modules';
+import "./SlideFilmCountry.css";
 
 export default function SlideFilmCountry() {
     return (
-        <div className="country-section">
-            <div className="country-sidebar">
-                <h2>
-                    Phim <span className="highlight">Hàn</span><br />
-                    Quốc <span className="highlight">mới</span>
-                </h2>
-                <a href="/" className="view-all">Xem toàn bộ <span>&gt;</span></a>
+        <div >
+            <div className="country-section">
+                <div className="country-sidebar">
+                    <h2>
+                        Phim <span className="highlight">Hàn</span><br />
+                        Quốc <span className="highlight">mới</span>
+                    </h2>
+
+                    <a href="/" className="view-all">Xem toàn bộ <span>&gt;</span></a>
+                </div>
+
+                <div className="country-slider">
+                    <Swiper
+                        modules={[Navigation]}
+                        navigation
+                        slidesPerView={3}
+                        spaceBetween={20}
+                    >
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
+                                </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
+                                </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
+                                </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
+                                </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
             </div>
+                <div className="country-section">
+                <div className="country-sidebar">
+                    <h2>
+                        Phim <span className="highlight">Hàn</span><br />
+                        Quốc <span className="highlight">mới</span>
+                    </h2>
 
-            <div className="country-slider">
-                <Swiper
-                    slidesPerView={3}
-                    spaceBetween={30}
-                    pagination={{
-                        type: 'fraction',
-                    }}
-                    navigation={true}
-                    modules={[Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    <SwiperSlide>
-                        <div className="movie-card">
-                            <div className="movie-image">
-                                <img
-                                    src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?w=1200"
-                                    alt="Trung Tâm Chăm Sóc Chấn Thương"
-                                />
-                                <div className="tags-container">
-                                    <span className="tag gray">PĐ. 8</span>
-                                    <span className="tag blue">LT. 8</span>
-                                </div>
-                            </div>
-                            <h3>Trung Tâm Chăm Sóc Chấn Thương</h3>
-                            <p>The Trauma Code: Heroes On Call</p>
-                        </div>
-                    </SwiperSlide>
+                    <a href="/" className="view-all">Xem toàn bộ <span>&gt;</span></a>
+                </div>
 
-                    <SwiperSlide>
-                        <div className="movie-card">
-                            <div className="movie-image">
-                                <img
-                                    src="https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?w=1200"
-                                    alt="Nữ Hoàng Nước Mắt"
-                                />
-                                <div className="tags-container">
-                                    <span className="tag gray">PĐ. 16</span>
+                <div className="country-slider">
+                    <Swiper
+                        modules={[Navigation]}
+                        navigation
+                        slidesPerView={3}
+                        spaceBetween={20}
+                    >
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
                                 </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
                             </div>
-                            <h3>Nữ Hoàng Nước Mắt</h3>
-                            <p>Queen of Tears</p>
-                        </div>
-                    </SwiperSlide>
+                        </SwiperSlide>
 
-                    <SwiperSlide>
-                        <div className="movie-card">
-                            <div className="movie-image">
-                                <img
-                                    src="https://images.unsplash.com/photo-1440404653325-ab127d49abc1?w=1200"
-                                    alt="Chuyên Gia Tuổi Trung Niên"
-                                />
-                                <div className="tags-container">
-                                    <span className="tag gray">PĐ. 6</span>
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
                                 </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
                             </div>
-                            <h3>Chuyên Gia Tuổi Trung Niên</h3>
-                            <p>Fifties Professionals</p>
-                        </div>
-                    </SwiperSlide>
+                        </SwiperSlide>
 
-                    <SwiperSlide>
-                        <div className="movie-card">
-                            <div className="movie-image">
-                                <img
-                                    src="https://images.unsplash.com/photo-1478720568477-152d9b164e26?w=1200"
-                                    alt="Moving"
-                                />
-                                <div className="tags-container">
-                                    <span className="tag gray">PĐ. 20</span>
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
                                 </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
                             </div>
-                            <h3>Moving</h3>
-                            <p>Korean Drama</p>
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="movie-card">
+                                <div className="movie-image">
+                                    <img src="https://media-cdn-v2.laodong.vn/storage/newsportal/2026/3/24/1674322/Nu-Hoang-Nuoc-Mat-1A-01.jpg"
+                                    />
+                                    <div className="tags-container">
+                                        <span className="tag gray">PĐ. 16</span>
+                                    </div>
+                                </div>
+                                <h3>Nữ Hoàng Nước Mắt</h3>
+                                <p>Queen of Tears</p>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
             </div>
         </div>
+
     );
 }
