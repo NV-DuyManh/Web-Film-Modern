6ac2ab4062ffaf6b781ba278b45f3c45499b406a
Nguyen Van Duy Manh <duymanhdnvn20092005@gmail.com>
2026-06-17 22:30:07 +0700

    Add Slide Film Country

--- src/pages/client/slide/SlideFilmCountry.jsx
+++ src/pages/client/slide/SlideFilmCountry.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useContext } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-
 import "swiper/css";
 import "swiper/css/navigation";
 
 import "./SlideFilmCountry.css";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import { useContext } from "react";
 import { MovieContext } from "../../../contexts/MovieProvider";
 import { getObjectById } from "../../../services/firebaseReponse";
 import { AuthorContext } from "../../../contexts/AuthorProvider";
@@ -18,6 +16,7 @@ export default function SlideFilmCountry() {
     const swiperRef = useRef(null);
     const movies = useContext(MovieContext);
     const authors = useContext(AuthorContext);
+
     useEffect(() => {
         if (swiperRef.current && swiperRef.current.params) {
             swiperRef.current.params.navigation.prevEl = prevRef.current;
@@ -26,68 +25,51 @@ export default function SlideFilmCountry() {
             swiperRef.current.navigation.update();
         }
     }, []);
+
     return (
         <div className="country-section md:flex">
             <div className="country-sidebar flex md:flex-col max-md:justify-between max-md:w-full">
                 <h2>
                     Phim <span className="highlight">Nhật</span><br /> Bản <span className="highlight">mới</span>
                 </h2>
-
                 <div className="view-all">Xem toàn bộ <span>&gt;</span></div>
             </div>
 
-            <div className="country-slider relative">
-                <button
-                    ref={prevRef}
-                    className="absolute left-0 top-29.5 z-10 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black/60 p-3 text-white hover:bg-black/80"
-                >
-                    <FaChevronLeft />
-                </button>
+            <div className="country-slider">
+                <div className="slider-wrapper">
+                    <button ref={prevRef} className="nav-btn nav-btn--prev">
+                        <FaChevronLeft />
+                    </button>
 
-                <Swiper
-                    modules={[Navigation]}
-                    onSwiper={(swiper) => (swiperRef.current = swiper)}
-                    breakpoints={{
-                        0: {
-                            slidesPerView: 1,
-                            spaceBetween: 10,
-                        },
-                        768: {
-                            slidesPerView: 2,
-                            spaceBetween: 15,
-                        },
-                        1024: {
-                            slidesPerView: 3,
-                            spaceBetween: 20,
-                        },
-                    }}
-                >
-                    {movies.map((e) =>
-                    (<SwiperSlide>
-                        <div className="movie-card">
-                            <div className="movie-image">
-                                <img src={e.imgUrl} />
-                                <div className="tags-container">
-                                    <span className="tag gray">{e.duration + " phút"}</span>
+                    <Swiper
+                        modules={[Navigation]}
+                        onSwiper={(swiper) => (swiperRef.current = swiper)}
+                        breakpoints={{
+                            0: { slidesPerView: 1, spaceBetween: 10 },
+                            768: { slidesPerView: 2, spaceBetween: 15 },
+                            1024: { slidesPerView: 3, spaceBetween: 20 },
+                        }}
+                    >
+                        {movies.map((e) => (
+                            <SwiperSlide key={e.id}>
+                                <div className="movie-card">
+                                    <div className="movie-image">
+                                        <img src={e.imgUrl} alt={e.name} draggable="false" />
+                                        <div className="tags-container">
+                                            <span className="tag gray">{e.duration + " phút"}</span>
+                                        </div>
+                                    </div>
+                                    <h3>{e.name}</h3>
+                                    <p>{getObjectById(authors, e.author)?.name}</p>
                                 </div>
-                            </div>
-                            <h3>{e.name}</h3>
-                            <p>
-                                {getObjectById(authors, e.author)?.name}
-                            </p>
-                        </div>
-                    </SwiperSlide>)
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
 
-                    )}
-
-                </Swiper>
-
-                <button
-                    ref={nextRef}
-                    className="absolute right-0 top-29.5 z-10 translate-x-1/2 -translate-y-1/2 rounded-full bg-black/60 p-3 text-white hover:bg-black/80"
-                >
-                    <FaChevronRight />
-                </button>
+                    <button ref={nextRef} className="nav-btn nav-btn--next">
+                        <FaChevronRight />
+                    </button>
+                </div>
             </div>
         </div>
     );
