1eefa934c6018d14ac755732c4f7342f9dbed654
Nguyen Van Duy Manh <duymanhdnvn20092005@gmail.com>
2026-06-28 21:16:42 +0700

    Update and Fix Actors Admin

--- src/pages/admin/entity/actors/ModalActor.jsx
+++ src/pages/admin/entity/actors/ModalActor.jsx
@@ -67,6 +67,7 @@ export default function ModalActor({ open, onChangeInput, handleClose, addactor,
                     disablePortal
                     options={COUNTRIES}
                     fullWidth
+                    classes={{ paper: 'neon-paper', listbox: 'neon-listbox', option: 'neon-option' }}
                     PopperProps={{ placement: "top-end" }}
                     value={actor.countriesID}
                     onChange={(e, value) => {
